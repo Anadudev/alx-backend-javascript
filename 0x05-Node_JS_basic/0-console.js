2f40548f1023c1bf96395f30c9d422f3ae6d71bb
Anadudev <anadudev@gmail.com>
2024-02-27 10:22:31 +0100

    function documentatioin

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,3 +1,8 @@
+/**
+ *displayMessage: a simple js function that
+ *	logs a message passed as a parameter
+ *@string {string} parameter to be logged
+ */
 function displayMessage(string){
 	console.log(string);
 }
