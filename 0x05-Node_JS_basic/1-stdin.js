38020e71955dca6be01fa31a06429ac3155bbe0a
Anadudev <anadudev@gmail.com>
2024-02-27 10:16:21 +0000

    debugging

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,12 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('data', (data) => {
+	const name = process.stdin.read();
+	if (name) {
+		console.log(`Your name is: ${name}`);
+	}
+});
+
+process.stdin.on('end', () => {
+	process.stdout.write('This important software is now closing\n');
+});
