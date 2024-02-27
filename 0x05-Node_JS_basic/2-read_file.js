8cca0f2a3281c71a75fb91f43cdb4df482ac2ed7
Anadudev <anadudev@gmail.com>
2024-02-27 14:49:51 +0000

    handling file erro and debuggingr

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,38 +1,37 @@
 const fs = require('fs');
 
 async function countStudents(path) {
-	if (!fs.existsSync(path) || !fs.statSync(path).isFile()) {
-		throw new Error('Cannot load the database');
-	}
+	try{
+		const data = fs.readFileSync(path, 'utf8').trim().split('\n').filter(Boolean);
 
-	const data = fs.readFileSync(path, 'utf8').trim().split('\n').filter(Boolean);
+		data.shift();
 
-	data.shift();
+		let total = 0;
+		let bySubjects = {};
 
-	let total = 0;
-	let bySubjects = {};
+		data.forEach(line => {
+			const [firstname, lastname, age, field] = line.split(',');
+			if (!firstname || !lastname || !age || !field) {
+				return;
+			}
+			if (bySubjects[field] === undefined) {
+				bySubjects[field] = { count: 1, list: [firstname] };
+			} else {
+				bySubjects[field].count++;
+				bySubjects[field].list.push(firstname);
+			}
+			total++;
+		});
 
-	data.forEach(line => {
-		const [firstname, lastname, age, field] = line.split(',');
-		if (!firstname || !lastname || !age || !field) {
-			return;
+		console.log(`Number of students: ${total}`);
+		for (const key in bySubjects) {
+			console.log(`Number of students in `+
+				`${key}: ${bySubjects[key].count}. `+
+				`List: ${bySubjects[key].list.join(', ')}`);
 		}
-		if (bySubjects[field] === undefined) {
-			bySubjects[field] = { count: 1, list: [firstname] };
-		} else {
-			bySubjects[field].count++;
-			bySubjects[field].list.push(firstname);
-		}
-		total++;
-	});
-
-	console.log(`Number of students: ${total}`);
-	for (const key in bySubjects) {
-		console.log(`Number of students in `+
-			`${key}: ${bySubjects[key].count}. `+
-			`List: ${bySubjects[key].list.join(', ')}`);
+	} catch (error){
+		throw new Error('Cannot load the database')
 	}
-
 }
 
 module.exports = countStudents;
