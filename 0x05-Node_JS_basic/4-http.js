10df072e2ee5084fde0b69c76a1f7b156dff59ae
Anadudev <anadudev@gmail.com>
2024-02-27 22:05:56 +0100

    exporting app

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,9 +2,11 @@ const http = require('node:http');
 const hostname = '127.0.0.1';
 const port = 1245;
 const app = http.createServer((req, res) => {
+  const body = 'Hello Holberton School!';
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!');
+  res.setHeader('Content-Length', body.length);
+  res.write(Buffer.from(body));
 });
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
