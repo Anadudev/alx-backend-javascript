f7c3e424cb0e402a8145a6f864399194c735b487
Anadudev <anadudev@gmail.com>
2024-02-27 16:51:03 +0100

    indentation  formatting

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -4,7 +4,7 @@
  *@message {message} parameter to be logged
  */
 const displayMessage = (message) => {
-	console.log(message);
+  console.log(message);
 };
 
 module.exports = displayMessage;
--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,37 +1,43 @@
-const fs = require('fs');
+const fs = require("fs");
 
 async function countStudents(path) {
-	try{
-		const data = fs.readFileSync(path, 'utf8').trim().split('\n').filter(Boolean);
+  try {
+    const data = fs
+      .readFileSync(path, "utf8")
+      .trim()
+      .split("\n")
+      .filter(Boolean);
 
-		data.shift();
+    data.shift();
 
-		let total = 0;
-		let bySubjects = {};
+    let total = 0;
+    let bySubjects = {};
 
-		data.forEach(line => {
-			const [firstname, lastname, age, field] = line.split(',');
-			if (!firstname || !lastname || !age || !field) {
-				return;
-			}
-			if (bySubjects[field] === undefined) {
-				bySubjects[field] = { count: 1, list: [firstname] };
-			} else {
-				bySubjects[field].count++;
-				bySubjects[field].list.push(firstname);
-			}
-			total++;
-		});
+    data.forEach((line) => {
+      const [firstname, lastname, age, field] = line.split(",");
+      if (!firstname || !lastname || !age || !field) {
+        throw new Error("Cannot load the database");
+      }
+      if (bySubjects[field] === undefined) {
+        bySubjects[field] = { count: 1, list: [firstname] };
+      } else {
+        bySubjects[field].count++;
+        bySubjects[field].list.push(firstname);
+      }
+      total++;
+    });
 
-		console.log(`Number of students: ${total}`);
-		for (const key in bySubjects) {
-			console.log(`Number of students in `+
-				`${key}: ${bySubjects[key].count}. `+
-				`List: ${bySubjects[key].list.join(', ')}`);
-		}
-	} catch (error){
-		throw new Error('Cannot load the database')
-	}
+    console.log(`Number of students: ${total}`);
+    for (const key in bySubjects) {
+      console.log(
+        `Number of students in ` +
+          `${key}: ${bySubjects[key].count}. ` +
+          `List: ${bySubjects[key].list.join(", ")}`
+      );
+    }
+  } catch (error) {
+    throw new Error("Cannot load the database");
+  }
 }
 
 module.exports = countStudents;
