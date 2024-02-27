2dcc5da1aaa3d70590f907370d03beb48ffb422b
Anadudev <anadudev@gmail.com>
2024-02-27 22:09:56 +0100

    node server setup

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,15 +1,14 @@
-const http = require('node:http');
-const hostname = '127.0.0.1';
+const http = require('http');
+
+const host = '127.0.0.1';
 const port = 1245;
+
 const app = http.createServer((req, res) => {
-  const body = 'Hello Holberton School!';
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', body.length);
-  res.write(Buffer.from(body));
+  res.end('Hello Holberton School!');
 });
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+
+app.listen(port, host, () => {});
 
 module.exports = app;
