16bbc44f298fa001cd1dd9e0b46248ab90c7844e
Anadudev <anadudev@gmail.com>
2024-02-27 10:44:29 +0000

    Handling input from the terminal using process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,14 @@
+process.stdin.setEncoding('utf8');
+
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-	const name = process.stdin.read();
-	if (name) {
-		console.log(`Your name is: ${name}`);
-	}
+process.stdin.on('readable', function() {
+  var chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
 process.stdin.on('end', () => {
-	process.stdout.write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
