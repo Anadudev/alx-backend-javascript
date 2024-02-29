f2c8a6ba8a4d87a39dae3835779d44fd1b884ae0
Anadudev <anadudev@gmail.com>
2024-02-29 11:10:07 +0000

    installing and setting up sinon

---
+++ 0x06-unittests_in_js/3-payment.js
@@ -0,0 +1,7 @@
+const calculateNumber = require('./utils').calculateNumber;
+
+
+function sendPaymentRequestToApi(totalAmount, totalShipping){
+	total = calculateNumber('SUM', totalAmount, totalShipping);
+	console.log(`The total is: ${total}`);
+}
