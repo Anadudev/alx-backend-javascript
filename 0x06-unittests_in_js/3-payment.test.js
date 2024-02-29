b6a82823f083d3b467810d955b986304c7b45583
Anadudev <anadudev@gmail.com>
2024-02-29 14:14:16 +0000

    correcting sinon testing

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,14 +1,23 @@
-const { expect } = require('chai');
-const util = require('./util').calculateNumber;
 const sinon = require('sinon');
-const sendPaymentRequestToApi = require('./3-payment.js').sendPaymentRequestToApi;
+const Utils = require('./utils');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./3-payment');
 
-describe('sendPaymentRequestToApi', function(){
-	it('Validating return values', function(){
-		const spy1 = sinon.spy(util);
-		const spy2 = sinon.spy(sendPaymentRequestToApi);
-		const a = spy1('SUM', 100, 20);
-		const b = spy2(100, 20);
-		expect(a).to.equal(b);
-	})
-})
+describe('sendPaymentRequestToApi', () => {
+  let calculateNumberSpy;
+
+  beforeEach(() => {
+    calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+  });
+
+  afterEach(() => {
+    calculateNumberSpy.restore();
+  });
+
+  it('Testing math used for sendPaymentRequestToApi is same as Utils.calculateNumber', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
+    expect(calculateNumberSpy.callCount).to.be.equal(1);
+  });
+});
+
