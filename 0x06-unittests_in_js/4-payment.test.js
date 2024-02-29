df99f56bb6e50589e37fd5f23f4ec1e907705f81
AnaduCG <godzionwin@gmail.com>
2024-02-29 21:31:04 +0100

    sinom stubs

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,31 @@
+const sinon = require('sinon');
+const { Utils } = require('./utils');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./4-payment');
+
+
+describe('sendPaymentRequestToApi', () => {
+
+    beforeEach(() => {
+        calNumStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    });
+
+    afterEach(() => {
+        sinon.restore();
+    });
+
+    it('Should accept two parameters', () => {
+        sendPaymentRequestToApi(100, 20);
+        expect(Utils.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    });
+    it('Should accept two parameters', () => {
+        sendPaymentRequestToApi(100, 20);
+        expect(Utils.calculateNumber.calledOnce).to.be.true;
+    });
+    it('should log correct data to the console', () => {
+        sinon.spy(console, 'log');
+        sendPaymentRequestToApi(100, 20);
+        expect(console.log.calledWith('The total is: 10')).to.be.true;
+    });
+});
+
