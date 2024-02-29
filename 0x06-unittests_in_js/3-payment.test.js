135756777cf774299e38a02d11b0485c63f29c6a
AnaduCG <godzionwin@gmail.com>
2024-02-29 21:12:58 +0100

    handling test cases

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,23 +1,31 @@
 const sinon = require('sinon');
-const Utils = require('./utils');
+const { calculateNumber } = require('./utils');
 const { expect } = require('chai');
 const sendPaymentRequestToApi = require('./3-payment');
 
+
 describe('sendPaymentRequestToApi', () => {
-  let calculateNumberSpy;
 
   beforeEach(() => {
-    calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+    sinon.spy(Utils, 'calculateNumber');
   });
 
   afterEach(() => {
-    calculateNumberSpy.restore();
+    sinon.restore();
   });
 
-  it('Testing math used for sendPaymentRequestToApi is same as Utils.calculateNumber', () => {
+  it('Should accept two parameters', () => {
     sendPaymentRequestToApi(100, 20);
-    expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
-    expect(calculateNumberSpy.callCount).to.be.equal(1);
+    expect(Utils.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+  });
+  it('Should accept two parameters', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(Utils.calculateNumber.calledOnce).to.be.true;
+  });
+  it('should log correct data to the console', () => {
+    sinon.spy(console, 'log');
+    sendPaymentRequestToApi(100, 20);
+    expect(console.log.calledWith('The total is: 120')).to.be.true;
   });
 });
 
